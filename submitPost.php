df474764b5df857a22f7d2bde8af53776ab4e186
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-10-24 13:56:56 -0500

    Create submitPost.php

---
+++ submitPost.php
@@ -0,0 +1,10 @@
+<?php require_once("header.php")?>
+
+
+<form action="postFilter.php" method="post">
+
+Prefix MIS or MGT: <input type="text" name="PrefixClass"><br>
+
+<input type="submit">
+    
+</form>
