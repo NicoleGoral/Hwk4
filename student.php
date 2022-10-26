08e5502822f2813dae1564e6dfda5defa7ab9e72
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-10-26 00:07:59 -0500

    Update student.php

--- student.php
+++ student.php
@@ -1,4 +1,15 @@
 <?php require_once("header.php"); ?>
+<h1>Students</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Student ID</th>
+      <th>Student Name</th>
+      <th>Grade Level</th>
+      <th>Course ID</th>
+    </tr>
+  </thead>
+  <tbody>
 <?php
 $servername = "localhost";
 $username = "ngoralou_homework3";
@@ -14,16 +25,16 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-      $sqlAdd = "insert into student (studentName, studentID, gradeLevel, courseID) value (?,?,?,?)";
+      $sqlAdd = "insert into student (studentID, studentName, gradeLevel, courseID) value (?,?,?,?)";
       $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("s", $_POST['sName']);
+      $stmtAdd->bind_param("sisi", $_POST['sID'], $_POST['sName'], $_POST['sGL'], $_POST['cID']);
       $stmtAdd->execute();
       echo '<div class="alert alert-success" role="alert">New Student added.</div>';
       break;
     case 'Edit':
-      $sqlEdit = "update student set studentName=? where studentID=?";
+      $sqlEdit = "update student set studentName=?, gradeLevel=?, courseID=? where studentID=?";
       $stmtEdit = $conn->prepare($sqlEdit);
-      $stmtEdit->bind_param("si", $_POST['sName'], $_POST['sID']);
+      $stmtEdit->bind_param("sisii", $_POST['sName'], $_POST['sGL'], $_POST['cID'], $_POST['sID']);
       $stmtEdit->execute();
       echo '<div class="alert alert-success" role="alert">Student edited.</div>';
       break;
@@ -36,23 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       break;
   }
 }
-?>
-    
-      <h1>Students</h1>
-      <table class="table table-striped">
-        <thead>
-          <tr>            
-            <th>Student ID</th>
-            <th>Student Name</th>
-            <th>Grade Level</th>
-            <th>Course ID</th>   
-            <th></th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          
+?>         
 <?php
 $sql = "SELECT studentID, studentName, gradeLevel, courseID FROM student";
 $result = $conn->query($sql);
@@ -68,13 +63,10 @@ if ($result->num_rows > 0) {
             <td><?=$row["gradeLevel"]?></td>
             <td><?=$row["courseID"]?></td>
             <td>
-            
-           </td>
-            <td>
               <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editStudent<?=$row["studentID"]?>">
                 Edit
               </button>
-              <div class="modal fade" id="editStudent<?=$row["studentName"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editStudent<?=$row["studentName"]?>Label" aria-hidden="true">
+              <div class="modal fade" id="editStudent<?=$row["studentID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editStudent<?=$row["studentID"]?>Label" aria-hidden="true">
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
@@ -84,11 +76,17 @@ if ($result->num_rows > 0) {
                     <div class="modal-body">
                       <form method="post" action="">
                         <div class="mb-3">
-                          <label for="editStudent<?=$row["studentID"]?>Name" class="form-label">Name</label>
-                          <input type="text" class="form-control" id="editStudent<?=$row["studentName"]?>Name" aria-describedby="editStudent<?=$row["studentID"]?>Help" name="sName" value="<?=$row['studentName']?>">
-                          <div id="editStudent<?=$row["studentName"]?>Help" class="form-text">Enter the Student's name.</div>
+                          <label for="editStudent<?=$row["studentID"]?>Name" class="form-label">Student Name</label>
+                          <input type="text" class="form-control" id="editStudent<?=$row["studentID"]?>Name" aria-describedby="editStudent<?=$row["studentID"]?>Help" name="sName" value="<?=$row['studentName']?>">
+                          <div id="editStudent<?=$row["studentID"]?>Help" class="form-text">Enter the Student's name.</div>
+                          <label for="gradeLevel" class="form-label">Grade Level</label>
+                          <input type="text" class="form-control" id="sGL" aria-describedby="nameHelp" name="sGL" value="<?=$row['gradeLevel']?>">
+                          <div id="nameHelp" class="form-text">Enter the Student's Grade Level</div>
+                          <label for="courseID" class="form-label">Course ID</label>
+                          <input type="text" class="form-control" id="cID" aria-describedby="nameHelp" name="cID" value="<?=$row['courseID']?>">
+                          <div id="nameHelp" class="form-text">Enter the course ID</div>
                         </div>
-                        <input type="hidden" name="sID" value="<?=$row['studentName']?>">
+                        <input type="hidden" name="sID" value="<?=$row['studentID']?>">
                         <input type="hidden" name="saveType" value="Edit">
                         <input type="submit" class="btn btn-primary" value="Submit">
                       </form>
@@ -127,24 +125,21 @@ $conn->close();
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="addStudentLabel">Add Student</h1>
+              <h1 class="modal-title fs-5" id="addCustomerLabel">Add Student</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
               <form method="post" action="">
                 <div class="mb-3">
-                  <label for="studentName" class="form-label">Name</label>
-                  <input type="text" class="form-control" id="supervisorName" aria-describedby="nameHelp" name="sName">
-                  <div id="nameHelp" class="form-text">Enter the student's name.</div>
-                  <label for="studentID" class="form-label">Student ID</label>
-                  <input type="text" class="form-control" id="studentID" aria-describedby="nameHelp" name="sID">
-                  <div id="nameHelp" class="form-text">Enter the student's ID.</div>
-                  <label for="gradeLevel" class="form-label">Grade Level</label>
-                  <input type="text" class="form-control" id="gradeLevel" aria-describedby="nameHelp" name="sGL">
-                  <div id="nameHelp" class="form-text">Enter the student's grade level.</div>
-                  <label for="courseID" class="form-label">Course ID</label>
-                  <input type="text" class="form-control" id="courseID" aria-describedby="nameHelp" name="cID">
-                  <div id="nameHelp" class="form-text">Enter the student's course ID.</div>
+                  <label for="studentName" class="form-label">Student Name</label>
+                  <input type="text" class="form-control" id="sName" aria-describedby="nameHelp" name="sName">
+                  <div id="nameHelp" class="form-text">Enter the Student's name.</div>
+                   <label for="gradeLevel" class="form-label">Grade Level</label>
+                   <input type="text" class="form-control" id="sGL" aria-describedby="nameHelp" name="sGL">
+                   <div id="nameHelp" class="form-text">Enter the Student's Grade Level</div>
+                          <label for="courseID" class="form-label">Course ID</label>
+                          <input type="text" class="form-control" id="cID" aria-describedby="nameHelp" name="cID">
+                          <div id="nameHelp" class="form-text">Enter the Course ID</div>
                 </div>
                 <input type="hidden" name="saveType" value="Add">
                 <button type="submit" class="btn btn-primary">Submit</button>
